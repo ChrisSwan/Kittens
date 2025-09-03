13b54866e83558a4b9be1f74e5f3cec59ffaeb7b
Chris Swan <chrisswan.webmail@googlemail.com>
2025-09-03 17:31:10 +0100

    V0.15: We now have welcome signs that show the name of the joining player.

--- PlayerManager.ts
+++ PlayerManager.ts
@@ -7,15 +7,30 @@ export class PlayerManager extends hz.Component<typeof PlayerManager> implements
     MainPlayerUI2: { type: hz.PropTypes.Entity },
     MainPlayerUI3: { type: hz.PropTypes.Entity },
     MainPlayerUI4: { type: hz.PropTypes.Entity },
+    Player1WelcomSign: { type: hz.PropTypes.Entity },
+    Player2WelcomSign: { type: hz.PropTypes.Entity },
+    Player3WelcomSign: { type: hz.PropTypes.Entity },
+    Player4WelcomSign: { type: hz.PropTypes.Entity },
   };
 
   mainPlayerUIs: hz.Entity[] = [];
+  playerWelcomeSigns: hz.TextGizmo[] = [];
 
   preStart(): void {
     // Assign the UIs from the prop types
     if (this.props.MainPlayerUI1 && this.props.MainPlayerUI2 && this.props.MainPlayerUI3 && this.props.MainPlayerUI4) {
       this.mainPlayerUIs = [this.props.MainPlayerUI1, this.props.MainPlayerUI2, this.props.MainPlayerUI3, this.props.MainPlayerUI4];
     }    
+    else {
+      console.error("Not all 4 UI HUDs have been connected to the PlayerManager")
+    }
+
+    if (this.props.Player1WelcomSign && this.props.Player2WelcomSign && this.props.Player3WelcomSign && this.props.Player4WelcomSign) {
+      this.playerWelcomeSigns = [this.props.Player1WelcomSign.as(hz.TextGizmo), this.props.Player2WelcomSign.as(hz.TextGizmo), this.props.Player3WelcomSign.as(hz.TextGizmo), this.props.Player4WelcomSign.as(hz.TextGizmo)];
+    }
+    else {
+      console.error("Not all 4 welcome signs have been connected to the PlayerManager")
+    }
 
     this.connectCodeBlockEvent(this.entity, hz.CodeBlockEvents.OnPlayerEnterWorld, (player: hz.Player) => {
       this.handleOnPlayerEnter(player);
@@ -32,11 +47,19 @@ export class PlayerManager extends hz.Component<typeof PlayerManager> implements
 //    if (!this.playerDataMap.has(player)) {
       this.assignMainPlayerUILocalScriptOwner(player, playerIndex);
 //    }
+
+
   }
 
   assignMainPlayerUILocalScriptOwner(player: hz.Player, scriptIndex: number): void {
     if (this.mainPlayerUIs[scriptIndex].owner.get() !== player) {
       this.mainPlayerUIs[scriptIndex].owner.set(player);
+    }
+    if (this.playerWelcomeSigns[scriptIndex].owner.get() !== player) {
+      this.playerWelcomeSigns[scriptIndex].owner.set(player);
+      this.playerWelcomeSigns[scriptIndex].text.set(player.name.get());
+      
+
     }
   }  
 }
